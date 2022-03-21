22a0fd14a92d1867fa56c79f65da7ca155401308
Adrianagazato <adrianabnoliveira@gmail.com>
2022-03-21 18:44:49 -0300

    Variaveis

---
+++ Modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,78 @@
+//Exercício 1
+//resultado do que será impresso e 10,5
+
+//Exercício 2
+//Resultado do que será impresso é 10 10 10
+
+
+//Exercćio 3 
+//Inicío do código
+//let horasDia = prompt("Quantas horas você trabalha por dia?")
+//let recebeDia = prompt("Quanto você recebe por dia?")
+//alert(`Você recebe ${ recebeDia/ horasDia} por hora`)
+//Fim do código
+
+//Exercício de escrita de código
+//Exercício 4
+// A - Declarar uma variável para armazenar um nome, sem atribuir um valor.
+let nome
+
+
+//B -declare uma variável para armazenar uma idade, sem atribuir um valor.
+let idade 
+
+//C - Imprimindo a ela dessa variavel usando o typeof
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+
+
+//D - o caso usamos o let para declarar a váriavel sem atribuição, e como não temos valor  Imprimiu na tela undefined.
+
+//E -   crie uma variável e atribua seu primeiro nome
+const nomePrimeiro = prompt ("Qual é o seu nome?")
+const idadeNome = prompt ("Qual é sua idade?")
+
+
+//F - Novamente Imprimindo, notei que apareceu meu nome e idade
+console.log(nomePrimeiro,idadeNome)
+
+//G - 
+
+console.log("Olá", nomePrimeiro , "vocẽ tem" , idadeNome , "anos")
+
+//Exercício 5
+
+//const vesAmarelo = prompt("Você esta usando uma roupa amarelo")
+//const vesazul = prompt("Você esta usando uma roupa azul")
+//const vesVermelho = prompt("Você esta usando uma roupa vermelho")
+let vesAmarelo = true
+console.log(typeof vesAmarelo)
+console.log(typeof vesVermelho)
+console.log(typeof vesAzul)
+
+//Exercício 6
+
+//Início do código 
+let a = 10
+let b = 25
+
+let c = a
+a = b
+b = c
+
+console.log(a)
+console.log(b)
+//Final o código
+
+//Desafio
+
+//let x = prompt("O primeiro número somado ao segundo número resulta em: x")
+//let y = prompt("O primeiro número multiplicado pelo segundo número resulta em: y.")
+let x = 5 + 2 
+let y = 6 * 2
+console.log(x,y)
+
+
+
