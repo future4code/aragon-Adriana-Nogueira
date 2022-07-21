9471cb68783271644c5c7c7f7e63090edc67be1e
Adrianagazato <adrianabnoliveira@gmail.com>
2022-07-21 18:19:20 -0300

    Finalizando o primeiro dia do projeto

---
+++ modulo4/projeto-labecommerce-backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { ping } from './endpoints/ping'
+import { cadastraUsuario } from "./endpoints/Cadastro"
+import { buscarUsers } from './endpoints/buscarUsers'
+
+dotenv.config()
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(process.env.PORT || 3003, () => {
+  console.log(`Servidor rodando na porta ${process.env.PORT || 3003}`)
+})
+
+app.get("/ping", ping)
+
+app.post("/user", cadastraUsuario)
+
+app.get("/user", buscarUsers)
