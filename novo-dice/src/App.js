d3e2582c94d78d8ef9cbfffa167a3770928758e6
adriananogueira <adrianabnoliveira@gmail.com>
2022-08-30 15:46:26 -0300

    Case Finalizado falta teste

---
+++ novo-dice/src/App.js
@@ -0,0 +1,17 @@
+
+
+import './App.css';
+import Dices from './components/Dice';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1 className='App-title'>Dice Game 🎲</h1>
+      </header>
+      <Dices />
+    </div>
+  );
+}
+
+export default App;
